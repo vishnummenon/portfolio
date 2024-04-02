a3ea655beabda07d281860bb01acae0eb19b23b7
Vishnu M Menon <vishnu.m@keyvalue.systems>
2024-04-02 15:01:22 +0530

    Revert

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    reactStrictMode: true,
-    basePath: '/portfolio'
-};
+const nextConfig = {};
 
 export default nextConfig;
