ea0132e1ffd3fadbd8429f4f5d71e88f19c0ea64
Vishnu M Menon <vishnu.m@keyvalue.systems>
2024-04-02 17:50:43 +0530

    Link Blog

--- app/components/Offline.tsx
+++ app/components/Offline.tsx
@@ -4,12 +4,27 @@ import BlogSnippet from "./BlogSnippet";
 
 const Offline: FC = () => {
   return (
-    <section id="offline" className="offline h-screen flex flex-col justify-center items-center">
+    <section
+      id="offline"
+      className="offline h-screen flex flex-col justify-center items-center"
+    >
       <h2 className="font-mono">I write, and therefore I am!</h2>
       <div className="my-20 w-full md:w-4/5">
-        <h6 className="font-mono italic">Check out some of the crowd favorite posts in my blog...</h6>
+        <h6 className="font-mono italic">
+          Check out some of the crowd favorite posts in{" "}
+          <a
+            href="https://canyouvish.blogspot.com"
+            target="_blank"
+            className="font-bold"
+          >
+            my blog
+          </a>
+          ...
+        </h6>
         <div className="flex justify-evenly flex-wrap my-8">
-        {blogPosts.map((blog) => <BlogSnippet key={blog.title} blog={blog} />)}
+          {blogPosts.map((blog) => (
+            <BlogSnippet key={blog.title} blog={blog} />
+          ))}
         </div>
       </div>
     </section>
